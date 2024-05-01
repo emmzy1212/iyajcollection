504ad17181bf6f711423b2ada03f9243e2c0404b
wgibbs3237 <wgibbs3237@gmail.com>
2024-05-01 16:58:33 +0100

    update on pop message

--- js/app.js
+++ js/app.js
@@ -1,42 +1,3 @@
-// let the html file work with the javascript function making sure when any img is clicked its should bring an alert the price of this item 
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Get the image element
-//     var img = document.getElementById('game');
-
-//     // Add click event listener to the image
-//     img.addEventListener('click', function() {
-//         // Check if the image ID is 'game'
-//         if (img.id === 'game') {
-//             // Alert the item price
-//             alert("This item's price is $10,000.");
-//             // Alert to pay to the bank account
-//             alert("Please pay to the bank account.");
-//         }
-//     });
-// });
-
-
-// let the html file work with the javascript function making sure when any img is clicked in this section  its should bring an alert("This item's price is $10,000."); amd another Alert to pay to the bank account alert("Please pay to the bank account.");
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Get all images in the section
-//     var images = document.querySelectorAll('.media__img img');
-
-//     // Add click event listener to each image
-//     images.forEach(function(img) {
-//         img.addEventListener('click', function() {
-//             // Alert the item price
-//             alert("This item's price is $10,000.");
-//             // Alert to pay to the bank account
-//             alert("Please pay to the bank account.");
-//         });
-//     });
-// });
-
 // add a pop that says to confirm if you are ready to purchase product, if yes its should pop up this javascript function and if no its should take us back to the home page
 // function showProductInfo(productName, price) {
 
@@ -62,8 +23,8 @@ function showProductInfo(productName, price) {
 
     // If user confirms, proceed with purchase
     if (confirmPurchase) {
-        alert("Pay to this bank account: 0000000 FCMB Ajoke");
-        alert("Send payment receipt");
+        alert("Talk to a representative & Make enquiry");
+        alert("Tel: +2349163861035 | Whatsapp: +2349163861035");
         location.href = "https://wa.me/2349163861035?text?";
     } else {
         // If user cancels, redirect to home page
